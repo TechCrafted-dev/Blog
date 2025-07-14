4c4a04af7c979005e0702f1f6a18ef6198956f2c
Sergio Trujillo <sergiotrujillonuez@gmail.com>
2025-07-14 17:13:01 +0200

    Remove ThemeToggle component, set light theme as default, and simplify dark mode configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // darkMode: 'selector', // Enable dark mode based on a class
+  darkMode: 'selector', // Enable dark mode based on a class
   content: ['src/**/*.{astro,html,js,ts,jsx,tsx,vue,svelte}'],
   theme: { extend: {} },
   plugins: [],
